86cf7d79dbc837f3576fec8f0bb9932e508c053d
eric <ericMicrosoft69@gmail.com>
2023-12-15 16:44:12 +0100

    identacion

--- src/ProcessLauncher.java
+++ src/ProcessLauncher.java
@@ -4,19 +4,18 @@ import java.io.IOException;
 
 public class ProcessLauncher {
 
-    public static void initMessagePrinter( String texto, String vocal, String outFileName){
-    ProcessBuilder processBuilder = new ProcessBuilder("java", "C:\\Users\\eric\\IdeaProjects\\practicaRecupperacionProcesos\\src\\VocalCounter.java",texto,vocal);
+    public static void initMessagePrinter(String texto, String vocal, String outFileName) {
+        ProcessBuilder processBuilder = new ProcessBuilder("java", "C:\\Users\\eric\\IdeaProjects\\practicaRecupperacionProcesos\\src\\VocalCounter.java", texto, vocal);
 
 
-    String rutaOutput = "C:\\Users\\eric\\IdeaProjects\\practicaRecupperacionProcesos\\src\\output";
+        String rutaOutput = "C:\\Users\\eric\\IdeaProjects\\practicaRecupperacionProcesos\\src\\output";
 
-    processBuilder.redirectOutput(new File(rutaOutput, outFileName));
+        processBuilder.redirectOutput(new File(rutaOutput, outFileName));
 
         try {
 
             Process ps = processBuilder.start();
             ps.waitFor();
-
 
 
         } catch (IOException e) {
